4f6ae50b035656c4063213d6d9dd2b9e8be28818
Jesus A Hernandez de Rojas <jesusangel.hernandezderojas@sky.uk>
2022-01-27 13:12:04 +0000

    feat!(): BREAKING CHANGE BREAKING CHANGE: kasjhdakjdhas

--- packages/package_1/index.js
+++ packages/package_1/index.js
@@ -1,6 +1,6 @@
 // this is a chore
 // chore chore\
-// chore chore
+// chore chore BREAK
 // chchchchch asd asd asd asd asd a
 // cchchchchchcssgit zxc`zcxcx a  xcv xcv xcv xcvd asdasd
 // chiscuhaiosucha dfsfdsdf s sdf sfd  sad asd asd 
