1846144156836ba5d31c9f67050cf96512a99f62
Jesus A Hernandez de Rojas <jesusangel.hernandezderojas@sky.uk>
2022-01-27 13:21:01 +0000

    fix(): bug fix

--- packages/package_1/index.js
+++ packages/package_1/index.js
@@ -4,6 +4,6 @@
 // chchchchch asd asd asd asd asd a
 // cchchchchchcssgit zxc`zcxcx a  xcv xcv xcv xcvd asdasd
 // chiscuhaiosucha dfsfdsdf s sdf sfd  sad asd asd
-// bug fix
+// bug fix sss
 console.log('1');
 console.log('2');
