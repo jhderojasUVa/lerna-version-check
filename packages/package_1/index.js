1556ab78e20eab117218506df54a771faff68d12
Jesus A Hernandez de Rojas <jesusangel.hernandezderojas@sky.uk>
2022-01-27 13:18:14 +0000

    fix(): something

--- packages/package_1/index.js
+++ packages/package_1/index.js
@@ -3,6 +3,7 @@
 // chore chore BREAK
 // chchchchch asd asd asd asd asd a
 // cchchchchchcssgit zxc`zcxcx a  xcv xcv xcv xcvd asdasd
-// chiscuhaiosucha dfsfdsdf s sdf sfd  sad asd asd 
+// chiscuhaiosucha dfsfdsdf s sdf sfd  sad asd asd
+// bug fix
 console.log('1');
 console.log('2');
